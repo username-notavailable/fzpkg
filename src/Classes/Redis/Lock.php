ed26d9487e85968eec624520f3b21e914d751568
fuzzy <viviani.demetrio82@gmail.com>
2025-02-10 08:17:34 +0100

    Aggiunta classe Lock

---
+++ src/Classes/Redis/Lock.php
@@ -0,0 +1,37 @@
+<?php
+
+class RedisLock
+{
+    static public function lock($redis, $key, $value = "true") 
+    {
+        $time = microtime(true);
+        $exit_time = $time + 10;
+        $sleep = 10000;
+
+        do 
+        {
+            // Lock Redis with PX and NX
+
+            $redis->multi();
+            $redis->set('lock:' . $key, $value, array('nx', 'ex' => 10));
+            $ret = $redis->exec();
+
+            if ($ret[0] == true) {
+                return true;
+            }
+
+            usleep($sleep);
+
+        } while (microtime(true) < $exit_time);
+
+        return false;
+    }
+
+    static public function unlock($redis, $key) 
+    {
+        $redis->multi();
+        $redis->del("lock:" . $key);
+        $redis->exec();
+
+    }
+}
