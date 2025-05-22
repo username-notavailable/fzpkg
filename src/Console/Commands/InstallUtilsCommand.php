0ebd816277c9bce0a5a3d6ad84989423a09700f3
Viviani Demetrio <d.viviani@fuzzy-net.it>
2025-05-22 19:26:55 +0200

    update install-utils command

--- src/Console/Commands/InstallUtilsCommand.php
+++ src/Console/Commands/InstallUtilsCommand.php
@@ -5,10 +5,11 @@ declare(strict_types=1);
 namespace Fuzzy\Fzpkg\Console\Commands;
 
 use Illuminate\Filesystem\Filesystem;
+use Fuzzy\Fzpkg\FzpkgServiceProvider;
 
 final class InstallUtilsCommand extends BaseCommand
 {
-    protected $signature = 'fz:install:utils';
+    protected $signature = 'fz:install:utils { --theme= : Selected theme (if themes enabled) }';
 
     protected $description = 'Install utilities';
 
@@ -18,16 +19,61 @@ final class InstallUtilsCommand extends BaseCommand
 
         $this->checkEnvFlag('FZ_UTILS_INSTALLED', 'Fz utils already installed');
 
+        $theme = '';
+
+        if (!empty($this->option('theme'))) {
+            if (!$filesystem->isDirectory(base_path('resources' . DIRECTORY_SEPARATOR . $this->option('theme')))) {
+                $this->fail('Theme "' . $this->option('theme') . '" not exists');
+            }
+            else {
+                $theme = $this->option('theme');
+            }
+        }
+
+        if (config('fz.load.cookies.theme') && empty($theme)) {
+            if (config('app.env') === 'production') {
+                $themes = FzpkgServiceProvider::getEnabledThemes();
+            }
+            else {
+                $themes = FzpkgServiceProvider::getAvailableThemes();
+            }
+
+            if (count($themes) === 1) {
+                $theme = $themes[0];
+            }
+            else {
+                $theme = $this->choice('Theme selection?',
+                    $themes,
+                    0,
+                    3
+                );
+            }
+        }
+
+        if (empty($theme)) {
+            $resourceSubPath = '';
+        }
+        else {
+            $resourceSubPath = $theme . DIRECTORY_SEPARATOR . 'resources' . DIRECTORY_SEPARATOR;
+        }
+
         $filesystem->copyDirectory(__DIR__.'/../../../data/utils/favicons', public_path());
 
-        $filesystem->ensureDirectoryExists(resource_path('js'));
-        $filesystem->copyDirectory(__DIR__.'/../../../data/utils/js', resource_path('js'));
+        $filesystem->ensureDirectoryExists(resource_path($resourceSubPath . 'js'));
+        $filesystem->copyDirectory(__DIR__.'/../../../data/utils/js', resource_path($resourceSubPath . 'js'));
 
-        $filesystem->ensureDirectoryExists(resource_path('sass'));
-        $filesystem->copyDirectory(__DIR__.'/../../../data/utils/sass', resource_path('sass'));
+        $filesystem->ensureDirectoryExists(resource_path($resourceSubPath . 'sass'));
+        $filesystem->copyDirectory(__DIR__.'/../../../data/utils/sass', resource_path($resourceSubPath . 'sass'));
 
-        $filesystem->copy(__DIR__.'/../../../data/utils/vite-theme.config.js', base_path('vite-theme.config.js'));
+        $viteConfigFile = base_path('vite-theme.config.js');
 
+        if (!$filesystem->exists($viteConfigFile)) {
+            $filesystem->copy(__DIR__.'/../../../data/utils/vite-theme.config.js', $viteConfigFile);
+        }
+        else {
+            $this->outLabelledInfo('"vite-theme.config.js" already exists, skipped...');
+        }
+        
         /* --- */
 
         $this->updateNodePackages(function ($packages) {
@@ -47,6 +93,11 @@ final class InstallUtilsCommand extends BaseCommand
 
         $this->updateEnvFileOrAppend($targets);
 
-        $this->outLabelledSuccess('Fuzzy utils installed');
+        if (empty($theme)) {
+            $this->outLabelledSuccess('Fuzzy utils installed into main resources directory');
+        }
+        else {
+            $this->outLabelledSuccess("Fuzzy utils installed for theme [$theme]");
+        }
     }
 }
